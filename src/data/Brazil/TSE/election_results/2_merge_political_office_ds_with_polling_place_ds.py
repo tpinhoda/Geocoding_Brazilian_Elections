70d3311affb759b6736a89686e0523ca7fa93418
Tiago Pinho <tiagopinhods@gmail.com>
2020-07-04 10:36:31 -0300

    Update 2_merge_political_office_ds_with_polling_place_ds.py

--- src/data/Brazil/TSE/election_results/2_merge_political_office_ds_with_polling_place_ds.py
+++ src/data/Brazil/TSE/election_results/2_merge_political_office_ds_with_polling_place_ds.py
@@ -2,48 +2,56 @@
 import warnings
 import pandas as pd
 import logging
-from os import listdir
+from os import listdir, environ
 from os.path import isfile, join
 from pathlib import Path
 from tqdm import tqdm
+from dotenv import load_dotenv, find_dotenv
 
 warnings.filterwarnings('ignore')
 
 
-def process_data(tse_input_filepath, geo_input_filepath, states_output_filepath, final_dataset_output_filepath):
-    """ Runs data processing scripts to turn interim election data data from (../interim) into
-        processed data with geocoding by polling place (saved in ../processed).
+def merge_data(election_results, polling_places, states_output_filepath, final_dataset_output_filepath):
+    """ Runs scripts to turn merge interim election data data from (../interim/election_results) with
+        polling places processed data from (../processed/polling_places).
+
+        The results of this script is saved in ../interim/election_results
     """
     logger = logging.getLogger(__name__)
-    logger.info('1 - Processing data')
-
+    logger.info('1 - Merging data')
     # Loading data with geocode information from polling places
-    data_geo = pd.read_csv(geo_input_filepath)
-
+    polling_places = pd.read_csv(polling_places)
     # Generating an id to facilitate merging
-    data_geo['id_polling_place'] = data_geo['SGL_UF'] + data_geo['LOCALIDADE_LOCAL_VOTACAO'] + data_geo['ZONA'].astype(str)  + data_geo['NUM_LOCAL'].astype(str)
+    polling_places['id_polling_place'] = polling_places['SGL_UF'] + polling_places['LOCALIDADE_LOCAL_VOTACAO'] + \
+                                         polling_places['ZONA'].astype(str) + polling_places['NUM_LOCAL'].astype(str)
 
     # Listing raw data
-    filenames = [filename for filename in listdir(tse_input_filepath) if isfile(join(tse_input_filepath, filename))]
-
+    filenames = [filename for filename in listdir(election_results) if isfile(join(election_results, filename))]
     list_state_df = []
     # For each state...
     for filename in tqdm(filenames):
-        # logger.info('Processing {} interim data'.format(filename))
-
         # Loading raw data
-        filepath = tse_input_filepath + filename
-        data_tse = pd.read_csv(filepath)
-
+        filepath = election_results + filename
+        results = pd.read_csv(filepath)
+        # Converting columns to string type
+        results['NR_ZONA'] = results['NR_ZONA'].astype(str)
+        results['NR_LOCAL_VOTACAO'] = results['NR_LOCAL_VOTACAO'].astype(str)
+        results['NR_SECAO'] = results['NR_SECAO'].astype(str)
         # Generating an ids to facilitate merging
-        data_tse['id_polling_place'] = data_tse['SG_ UF'] + data_tse['NM_MUNICIPIO'] + data_tse['NR_ZONA'].astype(str)  + data_tse['NR_LOCAL_VOTACAO'].astype(str)
-        data_tse['id_section'] = data_tse['SG_ UF'] + data_tse['NM_MUNICIPIO'] + data_tse['NR_ZONA'].astype(str)  + data_tse['NR_SECAO'].astype(str)
-        data_tse['id_zone'] = data_tse['SG_ UF'] + data_tse['NM_MUNICIPIO'] + data_tse['NR_ZONA'].astype(str)
-        data_tse['id_city'] = data_tse['SG_ UF'] + data_tse['NM_MUNICIPIO']
-
+        id_pl = results['SG_ UF'] + results['NM_MUNICIPIO'] + results['NR_ZONA'] + results['NR_LOCAL_VOTACAO']
+        id_sec = results['SG_ UF'] + results['NM_MUNICIPIO'] + results['NR_ZONA'] + results['NR_SECAO']
+        id_zone = results['SG_ UF'] + results['NM_MUNICIPIO'] + results['NR_ZONA']
+        id_city = results['SG_ UF'] + results['NM_MUNICIPIO']
+        # Imputing columns ids
+        results['id_polling_place'] = id_pl
+        results['id_section'] = id_sec
+        results['id_zone'] = id_zone
+        results['id_city'] = id_city
         # Merge datasets
-        merged = data_tse.merge(data_geo[['id_polling_place','COD_LOCALIDADE_IBGE','local_unico','lat','lon','geometry','rural','capital','precision', 'lev_dist', 'city_limits']], on='id_polling_place', how='left')
-
+        merged = results.merge(polling_places[
+                                   ['id_polling_place', 'COD_LOCALIDADE_IBGE', 'local_unico', 'lat', 'lon', 'geometry',
+                                    'rural', 'capital', 'precision', 'lev_dist', 'city_limits']],
+                               on='id_polling_place', how='left')
         # Save the data as csv
         merged.to_csv(states_output_filepath + filename, index=False)
 
@@ -53,7 +61,7 @@ def process_data(tse_input_filepath, geo_input_filepath, states_output_filepath,
     # Saving final dataset
     logger.info('2 - Saving final dataset...')
     final_df = pd.concat(list_state_df)
-    final_df.to_csv(final_dataset_output_filepath, index=False)
+    final_df.to_csv(final_dataset_output_filepath + '/Brazil.csv', index=False)
 
     logger.info('Done!')
 
@@ -61,25 +69,31 @@ def process_data(tse_input_filepath, geo_input_filepath, states_output_filepath,
 if __name__ == '__main__':
     # Project path
     project_dir = str(Path(__file__).resolve().parents[5])
-    # Set data parammeters
-    country = 'Brazil'
-    election_year = '2018'
+    # Find data.env automatically by walking up directories until it's found
+    dotenv_path = find_dotenv(filename='data.env')
+    # Load up the entries as environment variables
+    load_dotenv(dotenv_path)
+    # Get election results path
+    election_results_path = project_dir + environ.get('BRAZIL_ELECTION_RESULTS')
+    polling_places_path = project_dir + environ.get('BRAZIL_POLLING_PLACES')
+    # Set data parameters
+    year = '2018'
     office_folder = 'president'
     turn = '2'
+    # Generate input output paths
+    election_results_interim_path = election_results_path.format(year, 'interim')
+    polling_places_processed_path = polling_places_path.format(year, 'processed')
     # Set paths
-    tse_input_filepath = project_dir + '/data/interim/{}/TSE/election_data/{}/{}/turn_{}/states/'.format(country,election_year,office_folder,turn)
-    geo_input_filepath = project_dir + '/data/processed/{}/TSE/election_data/{}/polling_places/polling_places.csv'.format(country,election_year)
-    states_output_filepath = project_dir + '/data/processed/{}/TSE/election_data/{}/{}/turn_{}/states/'.format(country,election_year,office_folder,turn)
-    final_dataset_output_filepath = project_dir + '/data/processed/{}/TSE/election_data/{}/{}/turn_{}/data.csv'.format(country,election_year,office_folder,turn)
+    election_results_path = election_results_interim_path + '/{}/turn_{}/'.format(office_folder,
+                                                                                  turn)
+    polling_places_path = polling_places_processed_path + '/polling_places.csv'
     # Log text to show on screen
     log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logging.basicConfig(level=logging.INFO, format=log_fmt)
-
     # Print parameters
     print('======Parameters========')
-    print('Country: {}'.format(country))
-    print('Election year: {}'.format(election_year))
+    print('Election year: {}'.format(year))
     print('Office: {}'.format(office_folder))
     print('Turn: {}'.format(turn))
 
-    process_data(tse_input_filepath, geo_input_filepath, states_output_filepath, final_dataset_output_filepath)
+    merge_data(election_results_path, polling_places_path, election_results_path, election_results_path)
