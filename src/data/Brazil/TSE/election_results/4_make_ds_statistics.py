23092b082cb55278dd8cd1bc5ed993596dd1847a
Tiago Pinho <tiagopinhods@gmail.com>
2020-07-07 10:14:27 -0300

    Update 4_make_ds_statistics.py

--- src/data/Brazil/TSE/election_results/4_make_ds_statistics.py
+++ src/data/Brazil/TSE/election_results/4_make_ds_statistics.py
@@ -3,10 +3,11 @@ import warnings
 import logging
 import pandas as pd
 from pathlib import Path
-from os import mkdir, environ
+from os import environ
 from dotenv import load_dotenv, find_dotenv
 
 warnings.filterwarnings('ignore')
+
 
 def calculate_integrity(data):
     tse_score = sum(data['precision'] == 'TSE') * 1
@@ -36,89 +37,56 @@ def calculate_integrity(data):
     return final_score
 
 
-def generate_markdown_report(data,
-                             filtered_data,
-                             output_filepath,
-                             integrity_score,
-                             city_limits,
-                             levenstein_threshold,
-                             precision_categories,
-                             aggregate_level):
-    parameters_report = {'Aggregate level': aggregate_level,
-                         'City limits:': [city_limits],
-                         'Precision categories': [precision_categories],
-                         'Levenshtein threshold': levenstein_threshold}
-    report_df = pd.DataFrame(parameters_report)
-    parammeters_markdown = "## Filter Parameters \n" + report_df.to_markdown(showindex=False)
-
-    cities = len(filtered_data.groupby('CD_MUNICIPIO'))
-    cities_perc = 100 * cities / len(data.groupby('CD_MUNICIPIO'))
+def n_cities(original, cleaned):
+    cities = len(cleaned.groupby('CD_MUNICIPIO'))
+    cities_perc = 100 * cities / len(original.groupby('CD_MUNICIPIO'))
     cities = str(cities) + ' (' + str(round(cities_perc, 2)) + '%)'
 
-    left_cities = len(data.groupby('CD_MUNICIPIO')) - len(filtered_data.groupby('CD_MUNICIPIO'))
-    left_cities_perc = 100 * left_cities / len(data.groupby('CD_MUNICIPIO'))
+    left_cities = len(original.groupby('CD_MUNICIPIO')) - len(cleaned.groupby('CD_MUNICIPIO'))
+    left_cities_perc = 100 - cities_perc
     left_cities = str(left_cities) + ' (' + str(round(left_cities_perc, 2)) + '%)'
+    return {'represented': cities, 'left': left_cities}
 
-    rows = len(filtered_data)
-    rows_perc = 100 * rows / len(data)
+
+def n_aggr_level(original, cleaned):
+    rows = len(cleaned)
+    rows_perc = 100 * rows / len(original)
     rows = str(rows) + ' (' + str(round(rows_perc, 2)) + '%)'
 
-    left_rows = len(data) - len(filtered_data)
-    left_rows_perc = 100 * left_rows / len(data)
+    left_rows = len(original) - len(cleaned)
+    left_rows_perc = 100 * left_rows / len(original)
     left_rows = str(left_rows) + ' (' + str(round(left_rows_perc, 2)) + '%)'
+    return {'represented': rows, 'left': left_rows}
 
-    electorate_size = sum(filtered_data['QT_APTOS'])
-    electorate_size_perc = 100 * electorate_size / sum(data['QT_APTOS'])
-    electorate_size = str(electorate_size) + ' (' + str(round(electorate_size_perc, 2)) + '%)'
 
-    left_electorate_size = sum(data['QT_APTOS']) - sum(filtered_data['QT_APTOS'])
-    left_electorate_size_perc = 100 * left_electorate_size / sum(data['QT_APTOS'])
-    left_electorate_size = str(left_electorate_size) + ' (' + str(round(left_electorate_size_perc, 2)) + '%)'
+def n_attribute(original, cleaned, attr):
+    attr_size = sum(cleaned[attr])
+    attr_size_perc = 100 * attr_size / sum(original[attr])
+    attr_size = str(attr_size) + ' (' + str(round(attr_size_perc, 2)) + '%)'
 
-    turnout = sum(filtered_data['QT_COMPARECIMENTO'])
-    turnout_perc = 100 * turnout / sum(data['QT_COMPARECIMENTO'])
-    turnout = str(turnout) + ' (' + str(round(turnout_perc, 2)) + '%)'
+    left_attr_size = sum(original[attr]) - sum(cleaned[attr])
+    left_attr_size_perc = 100 * left_attr_size / sum(original[attr])
+    left_attr_size = str(left_attr_size) + ' (' + str(round(left_attr_size_perc, 2)) + '%)'
+    return {'represented': attr_size, 'left': left_attr_size}
 
-    left_turnout = sum(data['QT_COMPARECIMENTO']) - sum(filtered_data['QT_COMPARECIMENTO'])
-    left_turnout_perc = 100 * left_turnout / sum(data['QT_COMPARECIMENTO'])
-    left_turnout = str(left_turnout) + ' (' + str(round(left_turnout_perc, 2)) + '%)'
 
-    statistics_report = {'Cities': cities,
-                         '(Not Included) Cities': left_cities,
-                         aggregate_level: rows,
-                         '(Not Included) ' + aggregate_level: left_rows,
-                         'Electorate': electorate_size,
-                         '(Not Included) Electorate': left_electorate_size,
-                         'Turnout': turnout,
-                         '(Not Included) Turnout': left_turnout}
+def generate_parameters_report(aggr_level, city_lim, precisions, lev):
+    parameters_report = {'Aggregate level': aggr_level,
+                         'City limits:': [city_lim],
+                         'Precision categories': [precisions],
+                         'Levenshtein threshold': lev}
+    report_df = pd.DataFrame(parameters_report)
+    parameters_markdown = "## Filter Parameters \n" + report_df.to_markdown(showindex=False)
+    return parameters_markdown
 
-    nulo = sum(filtered_data['Nulo'])
-    nulo_perc = 100 * nulo / sum(data['Nulo'])
-    nulo = str(nulo) + ' (' + str(round(nulo_perc, 2)) + ')'
 
-    branco = sum(filtered_data['Branco'])
-    branco_perc = 100 * branco / sum(data['Branco'])
-    branco = str(branco) + ' (' + str(round(branco_perc, 2)) + '%)'
-
-    bozo = sum(filtered_data['JAIR BOLSONARO'])
-    bozo_perc = 100 * bozo / sum(data['JAIR BOLSONARO'])
-    bozo = str(bozo) + ' (' + str(round(bozo_perc, 2)) + '%)'
-
-    haddad = sum(filtered_data['FERNANDO HADDAD'])
-    haddad_perc = 100 * haddad / sum(data['FERNANDO HADDAD'])
-    haddad = str(haddad) + ' (' + str(round(haddad_perc, 2)) + '%)'
-
-    votes_report = {'Null': nulo,
-                    'Branco': branco,
-                    'Jair Bolsonaro': bozo,
-                    'Fernando Haddad': haddad}
-
-    tse = sum(filtered_data['precision'] == 'TSE')
-    rooftop = sum(filtered_data['precision'] == 'ROOFTOP')
-    r_interpolated = sum(filtered_data['precision'] == 'RANGE_INTERPOLATED')
-    g_center = sum(filtered_data['precision'] == 'GEOMETRIC_CENTER')
-    approximate = sum(filtered_data['precision'] == 'APPROXIMATE')
-    n_value = sum(filtered_data['precision'] == 'NO_VALUE')
+def generate_precisions_report(cleaned_data):
+    tse = sum(cleaned_data['precision'] == 'TSE')
+    rooftop = sum(cleaned_data['precision'] == 'ROOFTOP')
+    r_interpolated = sum(cleaned_data['precision'] == 'RANGE_INTERPOLATED')
+    g_center = sum(cleaned_data['precision'] == 'GEOMETRIC_CENTER')
+    approximate = sum(cleaned_data['precision'] == 'APPROXIMATE')
+    n_value = sum(cleaned_data['precision'] == 'NO_VALUE')
 
     precision_report = {'TSE': tse,
                         'Rooftop': rooftop,
@@ -126,52 +94,95 @@ def generate_markdown_report(data,
                         'Geometric center': g_center,
                         'Approximate': approximate,
                         'No value': n_value}
-
-    # print(statistics_report)
-    report_df = pd.DataFrame(statistics_report, index=[0])
-    statistics_markdown = "\n ## Summary \n" + report_df.to_markdown(showindex=False)
-
     report_df = pd.DataFrame(precision_report, index=[0])
     precisions_markdown = "\n ## Precisions \n" + report_df.to_markdown(showindex=False)
+    return precisions_markdown
+
+
+def generate_votes_report(original_data, cleaned_data, candidates):
+    null = n_attribute(original_data, cleaned_data, 'Nulo')
+    blank = n_attribute(original_data, cleaned_data, 'Branco')
+    votes_report = {'Null': null['represented'],
+                    'Branco': blank['represented']}
+    for c in candidates:
+        c_votes = n_attribute(original_data, cleaned_data, c.upper())
+        votes_report[c] = c_votes['represented']
 
     report_df = pd.DataFrame(votes_report, index=[0])
     votes_markdown = "\n ## Votes \n" + report_df.to_markdown(showindex=False)
-
-    final_report = '# Dataset: IS_{} \n'.format(round(integrity_score,
-                                                      5)) + parammeters_markdown + '\n' + statistics_markdown + '\n' + precisions_markdown + '\n' + votes_markdown
-
-    print(final_report, file=open(output_filepath + 'PER_' + str(round(integrity_score, 5)) + '/summary.md', 'w'))
-
-    if __name__ == '__main__':
-        # Project path
-        project_dir = str(Path(__file__).resolve().parents[5])
-        # Find data.env automatically by walking up directories until it's found
-        dotenv_path = find_dotenv(filename='data.env')
-        # Load up the entries as environment variables
-        load_dotenv(dotenv_path)
-        # Get election results path
-        path = project_dir + environ.get('BRAZIL_ELECTION_RESULTS')
-        # Set parameters
-        year = '2018'
-        office_folder = 'president'
-        turn = '2'
-        per = ''
-        candidates = ['JAIR BOLSONARO', 'FERNANDO HADDAD']
-        # Generate input output paths
-        processed_path = path.format(year, 'processed')
-        # Set path
-        path = processed_path + '/{}/turn_{}/PER_{}/'.format(office_folder, turn, per)
-        # Log text to show on screen
-        log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-        logging.basicConfig(level=logging.INFO, format=log_fmt)
-        # Print parameters
-        print('======Global parameters========')
-        print('Election year: {}'.format(year))
-        print('Office: {}'.format(office_folder))
-        print('Turn: {}'.format(turn))
-        print('PER: {}'.format(per))
-
-        generate_markdown_report()
+    return votes_markdown
 
 
+def generate_summary_report(original_data,
+                            cleaned_data,
+                            aggregate_level):
+    cities = n_cities(original_data, cleaned_data)
+    aggr_places = n_aggr_level(original_data, cleaned_data)
+    electorate = n_attribute(original_data, cleaned_data, 'QT_APTOS')
+    turnout = n_attribute(original_data, cleaned_data, 'QT_COMPARECIMENTO')
 
+    summary_report = {'Cities': cities['represented'],
+                      '(Not Included) Cities': cities['left'],
+                      aggregate_level: aggr_places['represented'],
+                      '(Not Included) ' + aggregate_level: aggr_places['left'],
+                      'Electorate': electorate['represented'],
+                      '(Not Included) Electorate': electorate['left'],
+                      'Turnout': turnout['represented'],
+                      '(Not Included) Turnout': turnout['left']}
+
+    report_df = pd.DataFrame(summary_report, index=[0])
+    summary_markdown = "\n ## Summary \n" + report_df.to_markdown(showindex=False)
+    return summary_markdown
+
+
+if __name__ == '__main__':
+    # Project path
+    project_dir = str(Path(__file__).resolve().parents[5])
+    # Find data.env automatically by walking up directories until it's found
+    dotenv_path = find_dotenv(filename='data.env')
+    # Load up the entries as environment variables
+    load_dotenv(dotenv_path)
+    # Get election results path
+    path = project_dir + environ.get('BRAZIL_ELECTION_RESULTS')
+    # Set parameters
+    year = '2018'
+    office_folder = 'president'
+    turn = '2'
+    per = '97.17885'
+    candidates = ['JAIR BOLSONARO', 'FERNANDO HADDAD']
+    # Set data filters
+    city_limits = ['in', 'boundary_0.01', 'boundary_0.02', 'boundary_0.03']
+    levenshtein_threshold = .0
+    precision_categories = ['TSE', 'ROOFTOP', 'GEOMETRIC_CENTER', 'RANGE_INTERPOLATED', 'APPROXIMATE']
+    aggregate_level = 'Polling place'
+    # Generate input output paths
+    interim_path = path.format(year, 'interim')
+    processed_path = path.format(year, 'processed')
+    # Set path
+    processed_data = processed_path + '/{}/turn_{}/PER_{}/'.format(office_folder, turn, per)
+    interim_data = interim_path + '/{}/turn_{}/'.format(office_folder, turn)
+    # Log text to show on screen
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    # Print parameters
+    print('======Global parameters========')
+    print('Election year: {}'.format(year))
+    print('Office: {}'.format(office_folder))
+    print('Turn: {}'.format(turn))
+    print('PER: {}'.format(per))
+    # Load original and cleaned data
+    original_df = pd.read_csv(interim_data + 'data.csv')
+    cleaned_df = pd.read_csv(processed_data + 'data.csv')
+
+    # Generate the reports
+    parameters_report = generate_parameters_report(aggregate_level, city_limits, precision_categories,
+                                                   levenshtein_threshold)
+    summary_report = generate_summary_report(original_df, cleaned_df, aggregate_level)
+    precisions_report = generate_precisions_report(cleaned_df)
+    votes_report = generate_votes_report(original_df, cleaned_df, candidates)
+
+    # Save the report
+    final_report = '# Data set: PER_{} \n'.format(
+        per) + parameters_report + '\n' + summary_report + '\n' + precisions_report + '\n' + votes_report
+
+    print(final_report, file=open(processed_data + 'summary.md', 'w'))
