1fc09843ad6c2c280a5383c4c8c0e22a7dfff159
Tiago Pinho <tiagopinhods@gmail.com>
2021-08-09 15:58:33 -0300

    aggregating and merge process done

--- src/results/interim.py
+++ src/results/interim.py
@@ -64,7 +64,7 @@ class Interim(Election):
             na_values=["#NULO#", -1, -3],
             low_memory=False,
         ).infer_objects()
-    
+
     def _read_locations_csv(self) -> pd.DataFrame:
         filepath = join(
             self._get_process_folder_path(state="processed"),
@@ -73,7 +73,6 @@ class Interim(Election):
             "locations_IBGE.csv",
         )
         self.__locations_data = pd.read_csv(filepath).infer_objects()
-        
 
     def _rename_cols(self) -> pd.DataFrame:
         """Rename columns"""
@@ -111,7 +110,7 @@ class Interim(Election):
                 "[GEO]_ID_POLLING_PLACE",
                 "[GEO]_ID_POLLING_SECTION",
             ],
-            inplace=True
+            inplace=True,
         )
 
     def _join_votes(self, votes: pd.DataFrame) -> pd.DataFrame:
@@ -129,19 +128,21 @@ class Interim(Election):
         self.__results_data = self.__results_data.join(votes)
         self.__results_data.reset_index(inplace=True)
         self.__results_data.drop("[ELECTION]_VOTES", axis=1, inplace=True)
-    
+
     def _drop_na_cols(self) -> pd.DataFrame:
         self.__results_data.dropna(axis=1, inplace=True)
-    
+
     def _drop_na_candidates(self) -> pd.DataFrame:
-        self.__results_data.dropna(subset=["[ELECTION]_ID_CANDIDATE"], axis=0, inplace=True)
-    
+        self.__results_data.dropna(
+            subset=["[ELECTION]_ID_CANDIDATE"], axis=0, inplace=True
+        )
+
     def _fill_na_electorate_biometry(self) -> pd.DataFrame:
         self.__results_data["[ELECTION]_ELECTORATE_BIOMETRIA"].fillna(0, inplace=True)
-    
+
     def _convert_cols_to_str(self) -> pd.DataFrame:
         self.__results_data.columns = self.__results_data.columns.astype(str)
-        
+
     def _structure_data(self):
         # List raw data
         raw_dir = join(self._get_state_folders_path(state="raw"), self.data_name)
@@ -159,111 +160,59 @@ class Interim(Election):
             self._drop_na_cols()
             # Save the data as csv
             self.__list_results_data.append(self.__results_data.copy())
-    
+
     def _concatenate_list_results_data(self) -> pd.DataFrame:
         self.__results_data = pd.concat(self.__list_results_data)
         self._convert_cols_to_str()
         self.__list_results_data = []
-    
-    def _create_aggregation_map(self) -> Dict[str, str]:
-        return {col: ("sum" if is_numeric_dtype(self.__results_data[col]) and "ID" not in col else "first") for col in self.__results_data.columns}
 
-    @staticmethod
-    def _concat_cols(row):
-        return "".join(row)
-    
+    def _create_aggregation_map(self) -> Dict[str, str]:
+        return {
+            col: (
+                "sum"
+                if is_numeric_dtype(self.__results_data[col]) and "ID" not in col
+                else "first"
+            )
+            for col in self.__results_data.columns
+        }
+
     def _get_merging_keys(self) -> List[str]:
-        merging_keys = {"polling_place": ["[GEO]_UF", "[GEO]_CITY", "[GEO]_POLLING_ZONE", ["GEO_POLLING_PLACE"]],
-                        "city": ["[GEO]_UF", "[GEO]_CITY"]}
+        merging_keys = {
+            "polling_place": [
+                "[GEO]_UF",
+                "[GEO]_CITY",
+                "[GEO]_POLLING_ZONE",
+                ["GEO_POLLING_PLACE"],
+            ],
+            "city": ["[GEO]_UF", "[GEO]_CITY"],
+        }
         return merging_keys[self.aggregation_level]
-    
+
+    def _aggregate_data(self) -> pd.DataFrame:
+        group_keys = self._get_merging_keys()
+        agg_map = self._create_aggregation_map()
+        self.__results_data = self.__results_data.groupby(by=group_keys).agg(agg_map)
+
+    def _get_not_common_cols(self) -> List[str]:
+        return [
+            col
+            for col in self.__locations_data.columns
+            if col not in self.__results_data.columns
+        ]
+
     def _merge_results_and_location_data(self):
         self.logger_info("Merging results and location data.")
         # Load data with geocode information from polling places
         self._read_locations_csv()
-        # Generate an id to facilitate merging
         merging_keys = self._get_merging_keys()
-        self.__results_data = self.__results_data.merge(self.__locations_data, on=merging_keys, how=left)
-        print(self.__results_data)
-        exit()
-        polling_places["id_polling_place"] = (
-            polling_places["SGL_UF"]
-            + polling_places["LOCALIDADE_LOCAL_VOTACAO"]
-            + polling_places["ZONA"].astype(str)
-            + polling_places["NUM_LOCAL"].astype(str)
+        self.__locations_data.set_index(merging_keys, inplace=True)
+        not_commom_cols = self._get_not_common_cols()
+        self.__results_data = self.__results_data.join(
+            self.__locations_data[not_commom_cols]
         )
 
-        # List interim data
-        filenames = [
-            filename
-            for filename in listdir(election_results)
-            if isfile(join(election_results, filename))
-        ]
-        list_state_df = []
-        # Merge each file in filenames
-        for filename in tqdm(filenames):
-            # Load interim data
-            filepath = election_results + filename
-            results = pd.read_csv(filepath)
-            # Delete interim file
-            remove(filepath)
-            # Convert columns to string type
-            results["NR_ZONA"] = results["NR_ZONA"].astype(str)
-            results["NR_LOCAL_VOTACAO"] = results["NR_LOCAL_VOTACAO"].astype(str)
-            results["NR_SECAO"] = results["NR_SECAO"].astype(str)
-            # Generate an ids to facilitate merging
-            id_pl = (
-                results["SG_ UF"]
-                + results["NM_MUNICIPIO"]
-                + results["NR_ZONA"]
-                + results["NR_LOCAL_VOTACAO"]
-            )
-            id_sec = (
-                results["SG_ UF"]
-                + results["NM_MUNICIPIO"]
-                + results["NR_ZONA"]
-                + results["NR_SECAO"]
-            )
-            id_zone = results["SG_ UF"] + results["NM_MUNICIPIO"] + results["NR_ZONA"]
-            id_city = results["SG_ UF"] + results["NM_MUNICIPIO"]
-            # Input columns ids
-            results["id_polling_place"] = id_pl
-            results["id_section"] = id_sec
-            results["id_zone"] = id_zone
-            results["id_city"] = id_city
-            # Merge datasets
-            merged = results.merge(
-                polling_places[
-                    [
-                        "id_polling_place",
-                        "COD_LOCALIDADE_IBGE",
-                        "local_unico",
-                        "lat",
-                        "lon",
-                        "geometry",
-                        "rural",
-                        "capital",
-                        "precision",
-                        "lev_dist",
-                        "city_limits",
-                    ]
-                ],
-                on="id_polling_place",
-                how="left",
-            )
-            # Save the data as csv (Does not make sense have redundant data)
-            # merged.to_csv(states_output_filepath + filename, index=False)
-
-            # Append to list
-            list_state_df.append(merged)
-
-        # Save final dataset
-        output_filepath = output_filepath + "data.csv"
-        logger.info("2 - Saving final dataset in:\n" + output_filepath)
-        final_df = pd.concat(list_state_df)
-        final_df.to_csv(output_filepath, index=False)
-        logger.info("Done!")
-        return final_df
+    def _save_results_data(self):
+        self.__results_data.to_csv("data.csv", index=False)
 
     def run(self):
         """Run process"""
@@ -273,7 +222,6 @@ class Interim(Election):
         self._make_folders()
         self._structure_data()
         self._concatenate_list_results_data()
-        print(self._create_aggregation_map())
-        exit()
-        #self._merge_results_and_location_data()
-        print(self.__results_data.columns.values)
+        self._aggregate_data()
+        self._merge_results_and_location_data()
+        self._save_results_data()
